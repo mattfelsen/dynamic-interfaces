7bc40a7e714c4babaaab0ae3012e64addfa592b7
samua407 <samua407@newschool.edu>
2014-02-24 16:02:35 -0500

    sending to server at 1 second intervals

--- week03_mechanic/node/gtfs.js
+++ week03_mechanic/node/gtfs.js
@@ -1,41 +1,53 @@
-// Load up HTTP module
-var http = require("http");
-// var fs = require("fs");
 
-// Load ProtoBuf.js, parse the schema, and build the main FeedMessage container object
-var ProtoBuf = require("protobufjs");
-var builder = ProtoBuf.loadProtoFile("nyct-subway.proto");
+var server = require("server");
 
-var root = builder.build("");
-var FeedMessage = root.transit_realtime.FeedMessage;
+var run = function(){
 
-// MTA Developer Key
-var mtaKey = "831ad07f2673a9d25f1960755314d527";
-var url = "http://datamine.mta.info/mta_esi.php?key="+mtaKey+"&feed_id=2";
-
-var stationID = "L02N"; // 6th Ave
-// var stationID = "L08"; // Bedford Av
-// var stationID = "L03"; // Union Square
-
-/*
-var data = fs.readFileSync("gtfs.pb");
-var message = FeedMessage.decode(data);
-
-if (!message.hasOwnProperty('entity')) return;
-
-for (var i = 0; i < message.entity.length; i++) {
-    var obj = message.entity[i].alert;
-
-    if (obj == null) continue;
-    //if (!obj.hasOwnProperty('stop_time_update')) continue;
-
-	console.log(JSON.stringify(obj));
-}
-*/
+	// Load up HTTP module
+	var http = require("http");
+	// var fs = require("fs");
 
 
-//time handlers
-var time = {
+
+
+	// Load ProtoBuf.js, parse the schema, and build the main FeedMessage container object
+	var ProtoBuf = require("protobufjs");
+	var builder = ProtoBuf.loadProtoFile("nyct-subway.proto");
+	
+	var root = builder.build("");
+	var FeedMessage = root.transit_realtime.FeedMessage;
+	
+	// MTA Developer Key
+	var mtaKey = "831ad07f2673a9d25f1960755314d527";
+	var url = "http://datamine.mta.info/mta_esi.php?key="+mtaKey+"&feed_id=2";
+	
+	var stationID = "L02N"; // 6th Ave
+	// var stationID = "L08"; // Bedford Av
+	// var stationID = "L03"; // Union Square
+	
+	/*
+	var data = fs.readFileSync("gtfs.pb");
+	var message = FeedMessage.decode(data);
+	
+	if (!message.hasOwnProperty('entity')) return;
+	
+	for (var i = 0; i < message.entity.length; i++) {
+	    var obj = message.entity[i].alert;
+	
+	    if (obj == null) continue;
+	    //if (!obj.hasOwnProperty('stop_time_update')) continue;
+	
+		console.log(JSON.stringify(obj));
+	}
+	*/
+
+
+
+
+
+
+	//time handlers
+	var time = {
 	
 	now: function () {
 		return new Date();
@@ -104,66 +116,64 @@ var time = {
 };
 
 
+	http.get(url, function(res) {
+	    var data = [];
+	
+	    res.on("data", function(chunk) {
+	        data.push(chunk);
+	    });
+	
+	    res.on("end", function() {
+	    	// Combine all the data chunks into a buffer and decode it
+	        data = Buffer.concat(data);
+	        var message = FeedMessage.decode(data);
+	        // console.log(message);
+	
+	        if (!message.hasOwnProperty('entity')) return;
+	
+	        for (var i = 0; i < message.entity.length; i++) {
+	            // var obj = message.entity[i].alert;
+	            var obj = message.entity[i].trip_update;
+	
+	            if (obj == null) continue;
+	            if (!obj.hasOwnProperty('stop_time_update')) continue;
+	
+	            // console.log(JSON.stringify(obj));
+	            // console.log(obj);
+	            
+	            for (var j = 0; j < obj.stop_time_update.length; j++) {
+	                // search for desired stop only
+	                var stop_id = obj.stop_time_update[j].stop_id;
+	                if (stop_id.substring(0, 4) == stationID) {
+	                    var arrival = obj.stop_time_update[j].arrival.time.low;
+	                    var d = new Date(arrival * 1000);
+	
+		                // console.log("i:", i, "id:", message.entity[i].id, "stop_id:", obj.stop_time_update[j].stop_id, "date:", d.toLocaleTimeString());
+						//console.log("stop_id:", obj.stop_time_update[j].stop_id, "date:", d.toLocaleTimeString());
+	
+						//get time in minutes until next train and push difference between now + arrival to time.diff array
+						var arrivalMins = d.getHours()*60 + d.getMinutes();
+						var diff = arrivalMins - time.full();
+						time.diff.push(diff);
+						
+	
+	                }
+	            }
+	            //console.log(JSON.stringify(myMessage.entity[i]) + ',');
+	            //if (i > 5) break;
+	            
+	            
+	        }	
+	      //console.log('The train is now coming at intervals of ' + time.interval(time.full()) + ' minutes.');
+	      //console.log('The next train is ' + time.diff[0] + ' minutes away.');		
+	      //console.log('Spokes 0 through ' + time.numSpokesLit() + ' should be lit.');
+	      server.send(time.numSpokesLit());
+	    });
+	    
+	});
+};
+		
 
-
-
-
-
-http.get(url, function(res) {
-    var data = [];
-
-    res.on("data", function(chunk) {
-        data.push(chunk);
-    });
-
-    res.on("end", function() {
-    	// Combine all the data chunks into a buffer and decode it
-        data = Buffer.concat(data);
-        var message = FeedMessage.decode(data);
-        // console.log(message);
-
-        if (!message.hasOwnProperty('entity')) return;
-
-        for (var i = 0; i < message.entity.length; i++) {
-            // var obj = message.entity[i].alert;
-            var obj = message.entity[i].trip_update;
-
-            if (obj == null) continue;
-            if (!obj.hasOwnProperty('stop_time_update')) continue;
-
-            // console.log(JSON.stringify(obj));
-            // console.log(obj);
-            
-            for (var j = 0; j < obj.stop_time_update.length; j++) {
-                // search for desired stop only
-                var stop_id = obj.stop_time_update[j].stop_id;
-                if (stop_id.substring(0, 4) == stationID) {
-                    var arrival = obj.stop_time_update[j].arrival.time.low;
-                    var d = new Date(arrival * 1000);
-
-	                // console.log("i:", i, "id:", message.entity[i].id, "stop_id:", obj.stop_time_update[j].stop_id, "date:", d.toLocaleTimeString());
-					//console.log("stop_id:", obj.stop_time_update[j].stop_id, "date:", d.toLocaleTimeString());
-
-					//get time in minutes until next train and push difference between now + arrival to time.diff array
-					var arrivalMins = d.getHours()*60 + d.getMinutes();
-					var diff = arrivalMins - time.full();
-					time.diff.push(diff);
-					
-
-                }
-            }
-            //console.log(JSON.stringify(myMessage.entity[i]) + ',');
-            //if (i > 5) break;
-            
-            
-        }
-
-        console.log('The train is now coming at intervals of ' + time.interval(time.full()) + ' minutes.');
-        console.log('The next train is ' + time.diff[0] + ' minutes away.');
-        //console.log('The next next train is ' + time.diff[1] + ' minutes away.');
-        console.log('Spokes 0 through ' + time.numSpokesLit() + ' should be lit.');
-    });
-    
-});
+setInterval(run, 1*1000);
 
 
