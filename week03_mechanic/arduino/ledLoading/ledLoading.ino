638a9c0c58f99dfc18058b81765602ef43801e60
Matt Felsen <mattfelsen@gmail.com>
2014-02-26 20:46:20 -0500

    Bugfix for freezing when setting all spokes to complete and cleanup

--- week03_mechanic/arduino/ledLoading/ledLoading.ino
+++ week03_mechanic/arduino/ledLoading/ledLoading.ino
@@ -2,7 +2,7 @@
 
 #define PIN 7
 #define NUM_SPOKES 12
-#define SPOKES_LIT 0
+#define SPOKES_LIT 6
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(103, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -24,7 +24,6 @@ int spokes[NUM_SPOKES][2] = {
 void setup() {
   strip.begin();
   strip.show();
-//  Serial.begin(9600);
 }
 
 void loop() {
@@ -37,29 +36,32 @@ void loop() {
 }
 
 void setCompletedSpokes(uint16_t number) {
-  // loop through spokes 0-number and set them a solid color
-  for (uint16_t i = 0; i < number; i++) {
-    for (uint16_t j = spokes[i][0]; j <= spokes[i][1]; j++) {
-      strip.setPixelColor(j, 0, 200, 200);
+  // loop through number of completed spokes and set them to solid
+  // we actually start from spoke 12 and go down so that they fill in
+  // clockwise, the same direction of the spinner  for (int i = 0; i < number; i++) {
+  for (int i = 0; i < number; i++) {
+    int currentSpoke = NUM_SPOKES - 1 - i;
+    for (uint16_t j = spokes[currentSpoke][0]; j <= spokes[currentSpoke][1]; j++) {
+      strip.setPixelColor(j, 0, 255, 255);
     }
   }
 }
 
 void setLoadingSpokes(uint16_t number) {
 
-  // loop through spokes number-end and set them to a color
+  // loop through spokes from number-end and set them to a color
   // with brightness determined by a sine wave
-  
+
   // update time & step variables
   float time = millis() / 100.0;
   float step = TWO_PI / (NUM_SPOKES - number);
-  
+
   // make 'em pulse
-  for (uint16_t i = number; i < NUM_SPOKES; i++) {
+  for (int i = NUM_SPOKES - number - 1; i >= 0; i--) {
 
     float sinVal = sin(time + step * i);
     float sinNormalized = sinVal / 2 + 0.5;
-    
+
     // this line "compressess" the sine wave so there's a bigger
     // difference between first/brightest spoke and the last/dimmest one
     sinNormalized = sinNormalized * sinNormalized * sinNormalized;
